c7a310147ee97deb0c9ecaa0bbf54e648553c5d6
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-10-03 09:12:02 -0400

    Finish adding operations

--- include/mini-qiskit/operations.hpp
+++ include/mini-qiskit/operations.hpp
@@ -9,7 +9,6 @@
     QuantumState object.
 */
 
-
 namespace mqis
 {
 
@@ -30,6 +29,40 @@ constexpr void superpose_states(QuantumState& state, std::size_t i0, std::size_t
 
     state[i0] = Complex {real_add, imag_add};
     state[i1] = Complex {real_sub, imag_sub};
+}
+
+constexpr void turn_states(QuantumState& state, std::size_t i0, std::size_t i1, double theta)
+{
+    const auto& state0 = state[i0];
+    const auto& state1 = state[i1];
+
+    const auto cost = std::cos(theta / 2.0);
+    const auto sint = std::sin(theta / 2.0);
+
+    const auto real0 = state0.real * cost + state1.imag * sint;
+    const auto imag0 = state0.imag * cost - state1.real * sint;
+    const auto real1 = state1.real * cost + state0.imag * sint;
+    const auto imag1 = state1.imag * cost - state0.real * sint;
+
+    state[i0] = Complex {real0, imag0};
+    state[i1] = Complex {real1, imag1};
+}
+
+constexpr void phaseturn_states(QuantumState& state, std::size_t i0, std::size_t i1, double theta)
+{
+    const auto& state0 = state[i0];
+    const auto& state1 = state[i1];
+
+    const auto cost = std::cos(theta / 2.0);
+    const auto sint = std::sin(theta / 2.0);
+
+    const auto real0 = state0.real * cost + state0.imag * sint;
+    const auto imag0 = state0.imag * cost - state0.real * sint;
+    const auto real1 = state1.real * cost - state1.imag * sint;
+    const auto imag1 = state1.imag * cost + state1.real * sint;
+
+    state[i0] = Complex {real0, imag0};
+    state[i1] = Complex {real1, imag1};
 }
 
 }  // namespace mqis
---
+++ test/source/operations_test.cpp
@@ -0,0 +1,91 @@
+#include <cmath>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include "mini-qiskit/operations.hpp"
+#include "mini-qiskit/state.hpp"
+
+constexpr static auto ABS_TOL = double {1.0e-6};
+
+// WARNING: BUG
+// - the logical operators don't take ownership of the Matcher objects
+// - so when you write
+//     "Catch::Matchers::WithinRel(value, REL_TOL) || Catch::Matchers::WithinAbs(value, ABS_TOL)"
+//   - the object for WithinRel ends up pointing to junk
+// auto within_either(double value) -> const auto
+// {
+//     return Catch::Matchers::WithinRel(value, REL_TOL) || Catch::Matchers::WithinAbs(value, ABS_TOL);
+// }
+
+TEST_CASE("Swap states operation")
+{
+    // Initial state: |00> = {{1.0, 0.0}, {0.0, 0.0}}
+    auto quantum_state = mqis::QuantumState {
+        {{1.0, 0.0}, {0.0, 0.0}}
+    };
+
+    mqis::swap_states(quantum_state, 0, 1);
+
+    const auto& coeff_0 = quantum_state[0];
+    const auto& coeff_1 = quantum_state[1];
+
+    REQUIRE_THAT(coeff_0.real, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+    REQUIRE_THAT(coeff_0.imag, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+    REQUIRE_THAT(coeff_1.real, Catch::Matchers::WithinAbs(1.0, ABS_TOL));
+    REQUIRE_THAT(coeff_1.imag, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+}
+
+TEST_CASE("Superpose states operation")
+{
+    // Initial state: superposition: |+> = {{1.0/sqrt2, 0.0}, {1.0/sqrt2, 0.0}}
+    auto quantum_state = mqis::QuantumState {
+        {{M_SQRT1_2, 0.0}, {M_SQRT1_2, 0.0}}
+    };
+
+    mqis::superpose_states(quantum_state, 0, 1);
+
+    const auto& coeff_0 = quantum_state[0];
+    const auto& coeff_1 = quantum_state[1];
+
+    REQUIRE_THAT(coeff_0.real, Catch::Matchers::WithinAbs(1.0, ABS_TOL));
+    REQUIRE_THAT(coeff_0.imag, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+    REQUIRE_THAT(coeff_1.real, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+    REQUIRE_THAT(coeff_1.imag, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+}
+
+TEST_CASE("Turn states operation")
+{
+    // Initial state: |00> = {{1.0, 0.0}, {0.0, 0.0}}
+    auto quantum_state = mqis::QuantumState {
+        {{1.0, 0.0}, {0.0, 0.0}}
+    };
+
+    mqis::turn_states(quantum_state, 0, 1, M_PI);
+
+    const auto& coeff_0 = quantum_state[0];
+    const auto& coeff_1 = quantum_state[1];
+
+    REQUIRE_THAT(coeff_0.real, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+    REQUIRE_THAT(coeff_0.imag, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+    REQUIRE_THAT(coeff_1.real, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+    REQUIRE_THAT(coeff_1.imag, Catch::Matchers::WithinAbs(-1.0, ABS_TOL));
+}
+
+TEST_CASE("Phase turn states operation")
+{
+    // Initial state: |+> = {{1.0/sqrt2, 0.0}, {1.0/sqrt2, 0.0}}
+    auto quantum_state = mqis::QuantumState {
+        {{M_SQRT1_2, 0.0}, {M_SQRT1_2, 0.0}}
+    };
+
+    mqis::phaseturn_states(quantum_state, 0, 1, M_PI);
+
+    const auto& coeff_0 = quantum_state[0];
+    const auto& coeff_1 = quantum_state[1];
+
+    REQUIRE_THAT(coeff_0.real, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+    REQUIRE_THAT(coeff_0.imag, Catch::Matchers::WithinAbs(-M_SQRT1_2, ABS_TOL));
+    REQUIRE_THAT(coeff_1.real, Catch::Matchers::WithinAbs(0.0, ABS_TOL));
+    REQUIRE_THAT(coeff_1.imag, Catch::Matchers::WithinAbs(M_SQRT1_2, ABS_TOL));
+}
