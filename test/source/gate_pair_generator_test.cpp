a600bedaa5a5c34af34293adf7e815b21fdad5e8
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-10-11 11:50:49 -0400

    Add a less trivial test for the SingleQubitGatePairGenerator

--- test/source/gate_pair_generator_test.cpp
+++ test/source/gate_pair_generator_test.cpp
@@ -1,4 +1,7 @@
 #include <algorithm>
+#include <compare>
+#include <iostream>
+#include <string>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -8,8 +11,21 @@
 
 #include "mini-qiskit/gate_pair_generator.hpp"
 
-using BasisIndexMap = std::unordered_map<const char*, std::size_t>;
-using IndexPair = std::pair<std::size_t, std::size_t>;
+using BasisIndexMap = std::unordered_map<std::string, std::size_t>;
+
+struct IndexPair
+{
+    std::size_t index0;
+    std::size_t index1;
+
+    friend std::ostream& operator<<(std::ostream& os, const IndexPair& pair)
+    {
+        os << "(" << pair.index0 << ", " << pair.index1 << ")";
+        return os;
+    }
+
+    friend constexpr auto operator<=>(const IndexPair& left, const IndexPair& right) = default;
+};
 
 auto get_generated_index_pairs(mqis::SingleQubitGatePairGenerator& generator) -> std::vector<IndexPair>
 {
@@ -64,6 +80,89 @@ TEST_CASE("SingleQubitGatePairGenerator with two qubits")
         auto expected = std::vector<IndexPair> {
             IndexPair {coeff_map.at("00"), coeff_map.at("01")},
              IndexPair {coeff_map.at("10"), coeff_map.at("11")}
+        };
+
+        auto actual = get_generated_index_pairs(generator);
+
+        // there isn't really a guaranteed ordered, AFAIK
+        std::sort(expected.begin(), expected.end());
+        std::sort(actual.begin(), actual.end());
+
+        REQUIRE(expected == actual);
+    }
+}
+
+TEST_CASE("SingleQubitGatePairGenerator with three qubits")
+{
+    const auto n_qubits = std::size_t {3};
+
+    // with two qubits, the states are 000, 100, 010, 110, 001, 101, 011, 111
+    const auto coeff_map = BasisIndexMap {
+        {"000", 0},
+        {"100", 1},
+        {"010", 2},
+        {"110", 3},
+        {"001", 4},
+        {"101", 5},
+        {"011", 6},
+        {"111", 7},
+    };
+
+    SECTION("loop over qubit at index 0")
+    {
+        auto generator = mqis::SingleQubitGatePairGenerator {0, n_qubits};
+
+        REQUIRE(generator.size() == impl_mqis::pow_2_int(n_qubits - 1));
+
+        auto expected = std::vector<IndexPair> {
+            IndexPair {coeff_map.at("000"), coeff_map.at("100")},
+            IndexPair {coeff_map.at("010"), coeff_map.at("110")},
+            IndexPair {coeff_map.at("001"), coeff_map.at("101")},
+            IndexPair {coeff_map.at("011"), coeff_map.at("111")}
+        };
+
+        auto actual = get_generated_index_pairs(generator);
+
+        // there isn't really a guaranteed ordered, AFAIK
+        std::sort(expected.begin(), expected.end());
+        std::sort(actual.begin(), actual.end());
+
+        REQUIRE(expected == actual);
+    }
+
+    SECTION("loop over qubit at index 1")
+    {
+        auto generator = mqis::SingleQubitGatePairGenerator {1, n_qubits};
+
+        REQUIRE(generator.size() == impl_mqis::pow_2_int(n_qubits - 1));
+
+        auto expected = std::vector<IndexPair> {
+            IndexPair {coeff_map.at("000"), coeff_map.at("010")},
+            IndexPair {coeff_map.at("100"), coeff_map.at("110")},
+            IndexPair {coeff_map.at("001"), coeff_map.at("011")},
+            IndexPair {coeff_map.at("101"), coeff_map.at("111")}
+        };
+
+        auto actual = get_generated_index_pairs(generator);
+
+        // there isn't really a guaranteed ordered, AFAIK
+        std::sort(expected.begin(), expected.end());
+        std::sort(actual.begin(), actual.end());
+
+        REQUIRE(expected == actual);
+    }
+
+    SECTION("loop over qubit at index 2")
+    {
+        auto generator = mqis::SingleQubitGatePairGenerator {2, n_qubits};
+
+        REQUIRE(generator.size() == impl_mqis::pow_2_int(n_qubits - 1));
+
+        auto expected = std::vector<IndexPair> {
+            IndexPair {coeff_map.at("000"), coeff_map.at("001")},
+            IndexPair {coeff_map.at("100"), coeff_map.at("101")},
+            IndexPair {coeff_map.at("010"), coeff_map.at("011")},
+            IndexPair {coeff_map.at("110"), coeff_map.at("111")}
         };
 
         auto actual = get_generated_index_pairs(generator);
