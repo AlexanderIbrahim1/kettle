f8c783d00c77f36b4e09c24d4ac7c7a99e556427
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-03-03 16:56:07 -0500

    Add support for 2x2 complex matrices and some basic operations

---
+++ include/mini-qiskit/common/matrix2x2.hpp
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <complex>
+#include <cmath>
+
+#include "mini-qiskit/common/mathtools.hpp"
+
+namespace mqis
+{
+
+struct Matrix2X2
+{
+    std::complex<double> elem00;  // top-left
+    std::complex<double> elem01;  // top-right
+    std::complex<double> elem10;  // bottom-left
+    std::complex<double> elem11;  // bottom-right
+
+    constexpr auto operator*=(const Matrix2X2& other) noexcept -> Matrix2X2
+    {
+        const auto new00 = elem00 * other.elem00 + elem01 * other.elem10;
+        const auto new01 = elem00 * other.elem01 + elem01 * other.elem11;
+        const auto new10 = elem10 * other.elem00 + elem11 * other.elem10;
+        const auto new11 = elem10 * other.elem01 + elem11 * other.elem11;
+
+        elem00 = new00;
+        elem01 = new01;
+        elem10 = new10;
+        elem11 = new11;
+
+        return *this;
+    }
+
+    constexpr auto operator+=(const Matrix2X2& other) noexcept -> Matrix2X2
+    {
+        const auto new00 = elem00 + other.elem00;
+        const auto new01 = elem01 + other.elem01;
+        const auto new10 = elem10 + other.elem10;
+        const auto new11 = elem11 + other.elem11;
+
+        elem00 = new00;
+        elem01 = new01;
+        elem10 = new10;
+        elem11 = new11;
+
+        return *this;
+    }
+};
+
+constexpr auto operator*(Matrix2X2 lhs, const Matrix2X2& rhs) noexcept -> Matrix2X2
+{
+    lhs *= rhs;
+    return lhs;
+}
+
+constexpr auto operator+(Matrix2X2 lhs, const Matrix2X2& rhs) noexcept -> Matrix2X2
+{
+    lhs += rhs;
+    return lhs;
+}
+
+inline auto matrix_square_root(const Matrix2X2& mat) -> Matrix2X2
+{
+    // uses the following: https://en.wikipedia.org/wiki/Square_root_of_a_2_by_2_matrix#A_general_formula
+    // we use the solution with the positive roots of s and t
+    const auto tau = mat.elem00 + mat.elem11;
+    const auto delta = mat.elem00 * mat.elem11 - mat.elem01 * mat.elem10;
+
+    const auto s = std::sqrt(delta);
+    const auto t = std::sqrt(tau + 2.0 * s);
+
+    const auto new00 = (mat.elem00 + s) / t;
+    const auto new01 = mat.elem01 / t;
+    const auto new10 = mat.elem10 / t;
+    const auto new11 = (mat.elem11 + s) / t;
+
+    return {new00, new01, new10, new11};
+}
+
+constexpr auto conjugate_transpose(const Matrix2X2& mat) -> Matrix2X2
+{
+    const auto new00 = std::conj(mat.elem00);
+    const auto new01 = std::conj(mat.elem10);
+    const auto new10 = std::conj(mat.elem01);
+    const auto new11 = std::conj(mat.elem11);
+
+    return {new00, new01, new10, new11};
+}
+
+constexpr auto almost_eq(
+    const Matrix2X2& left,
+    const Matrix2X2& right,
+    double tolerance_sq = impl_mqis::COMPLEX_ALMOST_EQ_TOLERANCE_EQ
+) noexcept -> bool
+{
+    // clang-format off
+    return \
+        almost_eq(left.elem00, right.elem00, tolerance_sq) && \
+        almost_eq(left.elem10, right.elem10, tolerance_sq) && \
+        almost_eq(left.elem01, right.elem01, tolerance_sq) && \
+        almost_eq(left.elem11, right.elem11, tolerance_sq);
+    // clang-format on
+}
+
+}  // namespace mqis
---
+++ test/source/common/matrix2x2_test.cpp
@@ -0,0 +1,66 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include "mini-qiskit/common/matrix2x2.hpp"
+
+
+TEST_CASE("conjugate transpose")
+{
+    SECTION("real matrix")
+    {
+        const auto mat = mqis::Matrix2X2 {
+            {1.0, 0.0},
+            {2.0, 0.0},
+            {3.0, 0.0},
+            {4.0, 0.0},
+        };
+
+        const auto expected = mqis::Matrix2X2 {
+            {1.0, 0.0},
+            {3.0, 0.0},
+            {2.0, 0.0},
+            {4.0, 0.0},
+        };
+
+        const auto actual = mqis::conjugate_transpose(mat);
+        REQUIRE(mqis::almost_eq(actual, expected));
+    }
+
+    SECTION("complex matrix")
+    {
+        const auto mat = mqis::Matrix2X2 {
+            {1.0, 2.0},
+            {3.0, 4.0},
+            {5.0, 6.0},
+            {7.0, 8.0},
+        };
+
+        const auto expected = mqis::Matrix2X2 {
+            {1.0, -2.0},
+            {5.0, -6.0},
+            {3.0, -4.0},
+            {7.0, -8.0},
+        };
+
+        const auto actual = mqis::conjugate_transpose(mat);
+        REQUIRE(mqis::almost_eq(actual, expected));
+    }
+}
+
+TEST_CASE("square root of 2x2 matrix")
+{
+    SECTION("X gate")
+    {
+        const auto x_gate = mqis::Matrix2X2 {
+            {0.0, 0.0},
+            {1.0, 0.0},
+            {1.0, 0.0},
+            {0.0, 0.0}
+        };
+
+        const auto sqrt_x_gate = mqis::matrix_square_root(x_gate);
+        const auto squared = sqrt_x_gate * sqrt_x_gate;
+
+        REQUIRE(mqis::almost_eq(squared, x_gate));
+    }
+}
