08da2548c70901bc95f534c46221c12b8643cf65
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-03-12 01:27:55 -0400

    Make progress towards an AND multiplicity controlled circuit maker

--- include/mini-qiskit/circuit_operations/make_controlled_circuit.hpp
+++ include/mini-qiskit/circuit_operations/make_controlled_circuit.hpp
@@ -51,6 +51,18 @@ void check_control_qubit_is_not_a_mapped_qubit_(const Container& container, std:
 }
 
 template <impl_mqis::ContainerOfQubitIndices Container = std::initializer_list<std::size_t>>
+void check_no_overlap_between_control_qubits_and_mapped_qubits_(const Container& mapped_qubits, const Container& control_qubits)
+{
+    auto control_qubit_set = std::unordered_set<std::size_t> {control_qubits.begin(), control_qubits.end()};
+
+    for (auto mapped_qubit : mapped_qubits) {
+        if (control_qubit_set.contains(mapped_qubit)) {
+            throw std::runtime_error {"The control qubit indices cannot overlap with the mapped qubit indices"};
+        }
+    }
+}
+
+template <impl_mqis::ContainerOfQubitIndices Container = std::initializer_list<std::size_t>>
 void check_new_indices_fit_onto_new_circuit_(const Container& container, std::size_t control_qubit, std::size_t n_qubits_on_new_circuit)
 {
     // the additional '1' comes from the control qubit
@@ -70,27 +82,32 @@ void check_new_indices_fit_onto_new_circuit_(const Container& container, std::si
     }
 }
 
+template <impl_mqis::ContainerOfQubitIndices Container = std::initializer_list<std::size_t>>
+void check_new_indices_fit_onto_new_circuit_(const Container& mapped_qubits, const Container& control_qubits, std::size_t n_qubits_on_new_circuit)
+{
+    const auto get_size = impl_mqis::get_container_size;
+    const auto n_minimum_new_indices = get_size(mapped_qubits) + get_size(control_qubits);
+
+    if (n_minimum_new_indices > n_qubits_on_new_circuit) {
+        throw std::runtime_error {"The mapped qubits will not fit onto the new circuit"};
+    }
+
+    const auto is_outside_new_qubits = [&](std::size_t i) { return i >= n_qubits_on_new_circuit; };
+
+    if (std::any_of(control_qubits.begin(), control_qubits.end(), is_outside_new_qubits)) {
+        throw std::runtime_error {"The control qubit is outside the range of qubits on the new circuit."};
+    }
+
+    if (std::any_of(mapped_qubits.begin(), mapped_qubits.end(), is_outside_new_qubits)) {
+        throw std::runtime_error {"A mapped qubit was found to be outside the range of qubits on the new circuit."};
+    }
+}
+
 }  // namespace impl_mqis
 
 namespace mqis
 {
 
-/*
-PLAN:
-
-Take an existing QuantumCircuit instance, and make the execution of the entire circuit
-dependent on a control qubit outside of that circuit.
-
-The existing QuantumCircuit gets turned into a new QuantumCircuit instance.
-
-The function should take as arguments:
-  - the QuantumCircuit instance to be made controlled
-  - the control qubit
-  - the new qubit indices that the qubits of the current QuantumCircuit instance get mapped to
-
-Later I should make a multiplicity-controlled version of this function
-  - I would have to turn all the general gates into U-gates
-*/
 template <impl_mqis::ContainerOfQubitIndices Container = std::initializer_list<std::size_t>>
 inline auto make_controlled_circuit(
     const mqis::QuantumCircuit& subcircuit,
@@ -166,6 +183,92 @@ inline auto make_controlled_circuit(
             case Gate::M : {
                 throw std::runtime_error {"Cannot make a measurement gate controlled."};
             }
+        }
+    }
+
+    return new_circuit;
+}
+
+template <impl_mqis::ContainerOfQubitIndices Container = std::initializer_list<std::size_t>>
+inline auto make_multiplicity_controlled_circuit(
+    const mqis::QuantumCircuit& subcircuit,
+    std::size_t n_new_qubits,
+    const Container& control_qubits,
+    const Container& mapped_qubits
+) -> mqis::QuantumCircuit
+{
+    impl_mqis::check_valid_number_of_mapped_indices_(mapped_qubits, subcircuit);
+    impl_mqis::check_all_indices_are_unique_(mapped_qubits);
+    impl_mqis::check_all_indices_are_unique_(control_qubits);
+    impl_mqis::check_no_overlap_between_control_qubits_and_mapped_qubits_(mapped_qubits, control_qubits);
+    impl_mqis::check_new_indices_fit_onto_new_circuit_(mapped_qubits, control_qubits, n_new_qubits);
+
+    auto new_circuit = mqis::QuantumCircuit {n_new_qubits};
+
+    for (const auto& gate_info : subcircuit) {
+        switch (gate_info.gate)
+        {
+            case Gate::X : {
+                const auto original_target = impl_mqis::unpack_x_gate(gate_info);
+                const auto new_target = impl_mqis::get_container_index(mapped_qubits, original_target);
+                apply_multiplicity_controlled_u_gate(new_circuit, x_gate(), new_target, control_qubits);
+                break;
+            }
+            case Gate::RX : {
+                const auto [angle, original_target] = impl_mqis::unpack_rx_gate(gate_info);
+                const auto new_target = impl_mqis::get_container_index(mapped_qubits, original_target);
+                apply_multiplicity_controlled_u_gate(new_circuit, rx_gate(angle), new_target, control_qubits);
+                break;
+            }
+            case Gate::H : {
+                const auto original_target = impl_mqis::unpack_h_gate(gate_info);
+                const auto new_target = impl_mqis::get_container_index(mapped_qubits, original_target);
+                apply_multiplicity_controlled_u_gate(new_circuit, h_gate(), new_target, control_qubits);
+                break;
+            }
+// IDEA:
+// - redirect the `make_controlled_circuit()` for a single control qubit, to this function!
+//   - just make the container of control qubits a single qubit!
+// TODO: complete this part
+//            case Gate::CX : {
+//                const auto [original_control, original_target] = impl_mqis::unpack_cx_gate(gate_info);
+//                const auto new_control = impl_mqis::get_container_index(mapped_qubits, original_control);
+//                const auto new_target = impl_mqis::get_container_index(mapped_qubits, original_target);
+//                apply_multiplicity_controlled_u_gate(new_circuit, x_gate(), new_target, {control, new_control});
+//                break;
+//            }
+//            case Gate::CRX : {
+//                const auto [original_control, original_target, angle] = impl_mqis::unpack_crx_gate(gate_info);
+//                const auto new_control = impl_mqis::get_container_index(mapped_qubits, original_control);
+//                const auto new_target = impl_mqis::get_container_index(mapped_qubits, original_target);
+//                apply_multiplicity_controlled_u_gate(new_circuit, rx_gate(angle), new_target, {control, new_control});
+//                break;
+//            }
+//            case Gate::CP : {
+//                const auto [original_control, original_target, angle] = impl_mqis::unpack_cp_gate(gate_info);
+//                const auto new_control = impl_mqis::get_container_index(mapped_qubits, original_control);
+//                const auto new_target = impl_mqis::get_container_index(mapped_qubits, original_target);
+//                apply_multiplicity_controlled_u_gate(new_circuit, p_gate(angle), new_target, {control, new_control});
+//                break;
+//            }
+//            case Gate::U : {
+//                const auto [original_target, original_gate_index] = impl_mqis::unpack_u_gate(gate_info);
+//                const auto new_target = impl_mqis::get_container_index(mapped_qubits, original_target);
+//                const auto& matrix = subcircuit.unitary_gate(original_gate_index);
+//                new_circuit.add_cu_gate(matrix, control, new_target);
+//                break;
+//            }
+//            case Gate::CU : {
+//                const auto [original_control, original_target, original_gate_index] = impl_mqis::unpack_cu_gate(gate_info);
+//                const auto new_control = impl_mqis::get_container_index(mapped_qubits, original_control);
+//                const auto new_target = impl_mqis::get_container_index(mapped_qubits, original_target);
+//                const auto& matrix = subcircuit.unitary_gate(original_gate_index);
+//                apply_multiplicity_controlled_u_gate(new_circuit, matrix, new_target, {control, new_control});
+//                break;
+//            }
+//            case Gate::M : {
+//                throw std::runtime_error {"Cannot make a measurement gate controlled."};
+//            }
         }
     }
 
