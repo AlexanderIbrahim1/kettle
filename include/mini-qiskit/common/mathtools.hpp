250f0139ab7752c7d9d9c4dfecd8984a0e0e5afb
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-03-30 00:53:02 -0400

    Add function for turning flat indices into grid indices

--- include/mini-qiskit/common/mathtools.hpp
+++ include/mini-qiskit/common/mathtools.hpp
@@ -71,6 +71,46 @@ inline auto qubit_string_to_state_index(const std::string& computational_state) 
     return static_cast<std::size_t>(bits.to_ullong());
 }
 
+/*
+    given a grid of side lengths (size0, size1), find (i0, i1), where
+    
+        i_flat = i1 + i0 * size1
+*/
+inline auto flat_index_to_grid_indices_2d(
+    std::size_t i_flat,
+    std::size_t size1
+) -> std::tuple<std::size_t, std::size_t>
+{
+    const auto i0 = i_flat / size1;
+    i_flat -= (i0 * size1);
+
+    const auto i1 = i_flat;
+
+    return {i0, i1};
+}
+
+/*
+    given a grid of side lengths (size0, size1, size2), find (i0, i1, i2), where
+    
+        i_flat = i2 + i1 * size2 + i0 * size1 * size2
+*/
+inline auto flat_index_to_grid_indices_3d(
+    std::size_t i_flat,
+    std::size_t size1,
+    std::size_t size2
+) -> std::tuple<std::size_t, std::size_t, std::size_t>
+{
+    const auto i0 = i_flat / (size1 * size2);
+    i_flat -= (i0 * size1 * size2);
+
+    const auto i1 = i_flat / size2;
+    i_flat -= (i1 * size2);
+
+    const auto i2 = i_flat;
+
+    return {i0, i1, i2};
+}
+
 }  // namespace impl_mqis
 
 namespace mqis
--- test/source/common/mathtools_test.cpp
+++ test/source/common/mathtools_test.cpp
@@ -73,3 +73,58 @@ TEST_CASE("big endian and little endian flips")
         REQUIRE(output == pair.output);
     }
 }
+
+TEST_CASE("flat_index_to_grid_indices_2d")
+{
+    struct TestCase
+    {
+        std::size_t i0;
+        std::size_t i1;
+        std::size_t size1;
+    };
+
+    const auto grid_indices_to_flat_index = [](const TestCase& tc) {
+        return tc.i1 + tc.i0 * tc.size1;
+    };
+
+    const auto testcase = GENERATE(
+        TestCase {0, 0, 10},
+        TestCase {3, 5, 10},
+        TestCase {3, 7, 12}
+    );
+
+    const auto flat_index = grid_indices_to_flat_index(testcase);
+    const auto [i0_actual, i1_actual] = impl_mqis::flat_index_to_grid_indices_2d(flat_index, testcase.size1);
+
+    REQUIRE(i0_actual == testcase.i0);
+    REQUIRE(i1_actual == testcase.i1);
+}
+
+TEST_CASE("flat_index_to_grid_indices_3d")
+{
+    struct TestCase
+    {
+        std::size_t i0;
+        std::size_t i1;
+        std::size_t i2;
+        std::size_t size1;
+        std::size_t size2;
+    };
+
+    const auto grid_indices_to_flat_index = [](const TestCase& tc) {
+        return tc.i2 + tc.i1 * tc.size2 + tc.i0 * tc.size1 * tc.size2;
+    };
+
+    const auto testcase = GENERATE(
+        TestCase {0, 0, 0, 10, 20},
+        TestCase {3, 5, 7, 10, 20},
+        TestCase {1, 5, 2, 7, 9}
+    );
+
+    const auto flat_index = grid_indices_to_flat_index(testcase);
+    const auto [i0_actual, i1_actual, i2_actual] = impl_mqis::flat_index_to_grid_indices_3d(flat_index, testcase.size1, testcase.size2);
+
+    REQUIRE(i0_actual == testcase.i0);
+    REQUIRE(i1_actual == testcase.i1);
+    REQUIRE(i2_actual == testcase.i2);
+}
