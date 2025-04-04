7bbd1f83bff2c440761844a07d4e15d79a2dbec2
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-04-04 18:24:23 -0400

    Add ability to continue an interrupted simulation from a saved statevector file

--- example/shaeer_qpe_simulate_minimal.cpp
+++ example/shaeer_qpe_simulate_minimal.cpp
@@ -18,14 +18,15 @@
 
 static constexpr auto N_UNITARY_QUBITS_TWO_ROTOR = std::size_t {6};
 static constexpr auto N_UNITARY_QUBITS_THREE_ROTOR = std::size_t {9};
+static constexpr auto RUN_FROM_START_KEY = int {-1};
 
 struct CommandLineArguments
 {
     CommandLineArguments(int argc, char** argv)
     {
-        if (argc != 7) {
+        if (argc != 8) {
             throw std::runtime_error {
-                "./a.out n_ancilla_qubits n_rotors n_trotter_steps abs_gate_dirpath subcircuit_filename abs_statevector_output_filepath\n"
+                "./a.out n_ancilla_qubits n_rotors n_trotter_steps abs_gate_dirpath subcircuit_filename abs_output_dirpath i_continue\n"
             };
         }
 
@@ -36,7 +37,14 @@ struct CommandLineArguments
         n_trotter_steps = std::stoul(arguments[2]);
         abs_circuits_dirpath = std::filesystem::path {arguments[3]};
         subcircuit_filename = arguments[4];
-        abs_statevector_output_filepath = std::filesystem::path {arguments[5]};
+        abs_output_dirpath = std::filesystem::path {arguments[5]};
+        i_continue = std::stoi(arguments[6]);
+
+        if (i_continue <= -2) {
+            throw std::runtime_error {
+                "'i_continue' must be -1 (for running from start) or a non-negative integer"
+            };
+        }
 
         if (n_rotors == 2) {
             n_unitary_qubits = N_UNITARY_QUBITS_TWO_ROTOR;
@@ -56,7 +64,8 @@ struct CommandLineArguments
     std::size_t n_trotter_steps;
     std::filesystem::path abs_circuits_dirpath;
     std::string subcircuit_filename;
-    std::filesystem::path abs_statevector_output_filepath;
+    std::filesystem::path abs_output_dirpath;
+    int i_continue;
 };
 
 void simulate_subcircuit(
@@ -69,10 +78,16 @@ void simulate_subcircuit(
     mqis::simulate(circuit, statevector);
 }
 
+auto statevector_filename(int i) -> std::string
+{
+    return std::format("statevector.dat{}", i);
+}
+
 void simulate_unitary(
     const CommandLineArguments& args,
     mqis::QuantumState& statevector,
-    std::size_t i_control
+    std::size_t i_control,
+    int& count
 )
 {
     auto n_powers = 1ul << i_control;
@@ -88,9 +103,17 @@ void simulate_unitary(
     const auto circuit = mqis::read_tangelo_circuit(n_total_qubits, circuit_filepath, 0);
 
     for (std::size_t i {0}; i < n_powers; ++i) {
+        if (args.i_continue != RUN_FROM_START_KEY && count <= args.i_continue) {
+            ++count;
+            continue;
+        }
+
         for (std::size_t i_trotter_ {0}; i_trotter_ < args.n_trotter_steps; ++i_trotter_) {
             mqis::simulate(circuit, statevector);
         }
+
+        mqis::save_statevector(args.abs_output_dirpath / statevector_filename(count), statevector);
+        ++count;
     }
 }
 
@@ -108,19 +131,29 @@ auto main(int argc, char** argv) -> int
     }();
 
     const auto n_total_qubits = args.n_ancilla_qubits + args.n_unitary_qubits;
-    auto statevector = mqis::QuantumState {n_total_qubits};
+
+    auto statevector = [&]() {
+        if (args.i_continue == RUN_FROM_START_KEY) {
+            return mqis::QuantumState {n_total_qubits};
+        } else {
+            return mqis::load_statevector(args.abs_output_dirpath / statevector_filename(args.i_continue));
+        }
+    }();
 
     // simulate the initial circuit
-    simulate_subcircuit(args.abs_circuits_dirpath / "initial_circuit.dat", statevector, n_total_qubits);
-    simulate_subcircuit(args.abs_circuits_dirpath / "qft_circuit.dat", statevector, n_total_qubits);
+    if (args.i_continue == RUN_FROM_START_KEY) {
+        simulate_subcircuit(args.abs_circuits_dirpath / "initial_circuit.dat", statevector, n_total_qubits);
+        simulate_subcircuit(args.abs_circuits_dirpath / "qft_circuit.dat", statevector, n_total_qubits);
+    }
 
+    auto count = int {0};
     for (std::size_t i_control {0}; i_control < args.n_ancilla_qubits; ++i_control) {
-        simulate_unitary(args, statevector, i_control);
+        simulate_unitary(args, statevector, i_control, count);
     }
 
     simulate_subcircuit(args.abs_circuits_dirpath / "iqft_circuit.dat", statevector, n_total_qubits);
 
-    mqis::save_statevector(args.abs_statevector_output_filepath, statevector);
+    mqis::save_statevector(args.abs_output_dirpath / statevector_filename(count), statevector);
 
     return 0;
 }
