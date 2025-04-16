7b79c06dc4d65e78f22c5d2012584e5fb79cb0e5
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-04-16 19:46:20 -0400

    Make the conversion functions all constexpr

--- include/mini-qiskit/state/qubit_state_conversion.hpp
+++ include/mini-qiskit/state/qubit_state_conversion.hpp
@@ -11,35 +11,48 @@
 #include "mini-qiskit/state/bitstring_utils.hpp"
 #include "mini-qiskit/state/endian.hpp"
 
+namespace impl_mqis
+{
+
+constexpr auto bitstring_to_state_index_bigendian_(const std::string& bitstring) -> std::size_t
+{
+    auto output = std::size_t {0};
+
+    for (auto bitchar : bitstring) {
+        output <<= 1;
+        if (bitchar == '1') {
+            output += 1;
+        }
+    }
+
+    return output;
+}
+
+}  // namespace impl_mqis
+
 namespace mqis
 {
 
-inline auto bitstring_to_state_index(
+constexpr auto bitstring_to_state_index(
     const std::string& bitstring,
     mqis::QuantumStateEndian input_endian
 ) -> std::size_t
 {
     impl_mqis::check_bitstring_is_valid_nonmarginal_(bitstring);
 
-    constexpr auto n_bits = std::numeric_limits<std::size_t>::digits;
-    const auto bits = std::bitset<n_bits> {bitstring};
-
-    const auto big_endian_index = static_cast<std::size_t>(bits.to_ullong());
+    const auto big_endian_index = impl_mqis::bitstring_to_state_index_bigendian_(bitstring);
 
     if (input_endian == mqis::QuantumStateEndian::BIG) {
         return big_endian_index;
     }
-    else if (input_endian == mqis::QuantumStateEndian::LITTLE) {
+    else {
         const auto n_qubits = bitstring.size();
         const auto little_endian_index = impl_mqis::endian_flip_(big_endian_index, n_qubits);
         return little_endian_index;
     }
-    else {
-        throw std::runtime_error {"UNREACHABLE: invalid QuantumStateEndian provided\n"};
-    }
 }
 
-inline auto state_index_to_dynamic_bitset(
+constexpr auto state_index_to_dynamic_bitset(
     std::size_t i_state,
     std::size_t n_qubits,
     mqis::QuantumStateEndian input_endian
@@ -60,18 +73,15 @@ inline auto state_index_to_dynamic_bitset(
         if (input_endian == mqis::QuantumStateEndian::LITTLE) {
             dyn_bitset[i_qubit] = static_cast<std::uint8_t>((bit_position & i_state) != 0);
         }
-        else if (input_endian == mqis::QuantumStateEndian::BIG) {
-            dyn_bitset[n_qubits - i_qubit - 1] = static_cast<std::uint8_t>((bit_position & i_state) != 0);
-        }
         else {
-            throw std::runtime_error {"UNREACHABLE: invalid QuantumStateEndian provided\n"};
+            dyn_bitset[n_qubits - i_qubit - 1] = static_cast<std::uint8_t>((bit_position & i_state) != 0);
         }
     }
 
     return dyn_bitset;
 }
 
-inline auto dynamic_bitset_to_bitstring(const std::vector<std::uint8_t>& bits) -> std::string
+constexpr auto dynamic_bitset_to_bitstring(const std::vector<std::uint8_t>& bits) -> std::string
 {
     auto bitstring = std::string {};
     bitstring.reserve(bits.size());
@@ -88,7 +98,7 @@ inline auto dynamic_bitset_to_bitstring(const std::vector<std::uint8_t>& bits) -
     return bitstring;
 }
 
-inline auto state_index_to_bitstring(
+constexpr auto state_index_to_bitstring(
     std::size_t i_state,
     std::size_t n_qubits,
     mqis::QuantumStateEndian input_endian
@@ -98,7 +108,7 @@ inline auto state_index_to_bitstring(
     return dynamic_bitset_to_bitstring(dyn_bitset);
 }
 
-inline auto dynamic_bitset_to_state_index(
+constexpr auto dynamic_bitset_to_state_index(
     const std::vector<std::uint8_t>& dyn_bitset,
     mqis::QuantumStateEndian input_endian
 ) -> std::size_t
@@ -107,7 +117,7 @@ inline auto dynamic_bitset_to_state_index(
     return bitstring_to_state_index(bitstring, input_endian);
 }
 
-inline auto bitstring_to_dynamic_bitset(
+constexpr auto bitstring_to_dynamic_bitset(
     const std::string& bitstring
 ) -> std::vector<std::uint8_t>
 {
@@ -128,42 +138,42 @@ inline auto bitstring_to_dynamic_bitset(
     return dyn_bitset;
 }
 
-inline auto bitstring_to_state_index_little_endian(const std::string& bitstring) -> std::size_t
+constexpr auto bitstring_to_state_index_little_endian(const std::string& bitstring) -> std::size_t
 {
     return bitstring_to_state_index(bitstring, mqis::QuantumStateEndian::LITTLE);
 }
 
-inline auto bitstring_to_state_index_big_endian(const std::string& bitstring) -> std::size_t
+constexpr auto bitstring_to_state_index_big_endian(const std::string& bitstring) -> std::size_t
 {
     return bitstring_to_state_index(bitstring, mqis::QuantumStateEndian::BIG);
 }
 
-inline auto state_index_to_bitstring_little_endian(std::size_t i_state, std::size_t n_qubits) -> std::string
+constexpr auto state_index_to_bitstring_little_endian(std::size_t i_state, std::size_t n_qubits) -> std::string
 {
     return state_index_to_bitstring(i_state, n_qubits, mqis::QuantumStateEndian::LITTLE);
 }
 
-inline auto state_index_to_bitstring_big_endian(std::size_t i_state, std::size_t n_qubits) -> std::string
+constexpr auto state_index_to_bitstring_big_endian(std::size_t i_state, std::size_t n_qubits) -> std::string
 {
     return state_index_to_bitstring(i_state, n_qubits, mqis::QuantumStateEndian::BIG);
 }
 
-inline auto state_index_to_dynamic_bitset_little_endian(std::size_t i_state, std::size_t n_qubits) -> std::vector<std::uint8_t>
+constexpr auto state_index_to_dynamic_bitset_little_endian(std::size_t i_state, std::size_t n_qubits) -> std::vector<std::uint8_t>
 {
     return state_index_to_dynamic_bitset(i_state, n_qubits, mqis::QuantumStateEndian::LITTLE);
 }
 
-inline auto state_index_to_dynamic_bitset_big_endian(std::size_t i_state, std::size_t n_qubits) -> std::vector<std::uint8_t>
+constexpr auto state_index_to_dynamic_bitset_big_endian(std::size_t i_state, std::size_t n_qubits) -> std::vector<std::uint8_t>
 {
     return state_index_to_dynamic_bitset(i_state, n_qubits, mqis::QuantumStateEndian::BIG);
 }
 
-inline auto dynamic_bitset_to_state_index_little_endian(const std::vector<std::uint8_t>& dyn_bitset) -> std::size_t
+constexpr auto dynamic_bitset_to_state_index_little_endian(const std::vector<std::uint8_t>& dyn_bitset) -> std::size_t
 {
     return dynamic_bitset_to_state_index(dyn_bitset, mqis::QuantumStateEndian::LITTLE);
 }
 
-inline auto dynamic_bitset_to_state_index_big_endian(const std::vector<std::uint8_t>& dyn_bitset) -> std::size_t
+constexpr auto dynamic_bitset_to_state_index_big_endian(const std::vector<std::uint8_t>& dyn_bitset) -> std::size_t
 {
     return dynamic_bitset_to_state_index(dyn_bitset, mqis::QuantumStateEndian::BIG);
 }
