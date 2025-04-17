ae2c192d646c65f36a3deac6ef55455c8f208373
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-04-17 17:59:40 -0400

    Add more documentation for these two examples

--- example/grover_two_qubit.cpp
+++ example/grover_two_qubit.cpp
@@ -3,37 +3,43 @@
 #include <mini-qiskit/mini-qiskit.hpp>
 
 /*
-This is an example of Grover's algorithm with n = 2 qubits.
-The target state we are interested in finding is `|11>`
+    This file contains an example of Grover's algorithm with n = 2 qubits.
+    This code is based on the example found in:
+        https://github.com/Qiskit/textbook/blob/main/notebooks/ch-algorithms/grover.ipynb
 
-Useful resource: https://github.com/Qiskit/textbook/blob/main/notebooks/ch-algorithms/grover.ipynb
+    The target state we are interested in finding is `|11>`.
 */
 
 
 auto main() -> int
 {
+    // construct the circuit with the gates needed for the Grover algorithm
     auto circuit = mqis::QuantumCircuit {2};
 
     // initialize to uniform superposition of all states
     circuit.add_h_gate({0, 1});
 
-    // add the oracle, which marks the `|11>` state as negative
+    // add the oracle
+    // the target state is `|11>`, which we want to mark as negative; we can do this
+    // with a single CZ gate
     circuit.add_cz_gate(0, 1);
 
     // add the Grover diffuser operator
-    // - this leaves |00> positive, but leaves all other computational basis states negative
-    // - the two Z-gates followed by the CZ-gate accomplish this
+    // - this leaves |00> positive, but makes all other computational basis states negative
+    // - the two Z gates followed by the CZ gate accomplish this
     //
-    // NOTICE: only a single rotation is required!
+    // NOTICE: only a single rotation is required for this example
     circuit.add_h_gate({0, 1});
     circuit.add_z_gate({0, 1});
     circuit.add_cz_gate(0, 1);
     circuit.add_h_gate({0, 1});
 
-    auto state = mqis::QuantumState {"00"};
-    mqis::simulate(circuit, state);
+    // create the initial statevector, and propagate it through the circuit
+    auto statevector = mqis::QuantumState {"00"};
+    mqis::simulate(circuit, statevector);
 
-    const auto counts = mqis::perform_measurements_as_counts_marginal(state, 1024);
+    // get a map of the bitstrings to the counts
+    const auto counts = mqis::perform_measurements_as_counts(statevector, 1024);
 
     for (const auto& [bitstring, count] : counts) {
         std::cout << "(bitstring, count) = (" << bitstring << ", " << count << ")\n";
--- example/quantum_phase_estimation_t_gate.cpp
+++ example/quantum_phase_estimation_t_gate.cpp
@@ -1,12 +1,42 @@
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 
 #include <mini-qiskit/mini-qiskit.hpp>
 
+/*
+    This file contains a demonstration of how to use the library to perform
+    Quantum Phase Estimation (QPE).
 
+    For this explanation:
+      - the qubits used to represent the binary expansion of the exponent in the
+        eigenvalue, are referred to as the "register qubits".
+      - the qubits used to represent the unitary operator whose eigenvalue is being
+        calculated, are referred to as the "unitary qubits"
+
+    We choose T gate, represented by the unitary matrix:
+        [ 1    0                 ]
+        [ 0    exp(2 pi i (1/8)) ]
+    
+    This means the |1> state has an eigenvalue of exp(2 pi i (1/8)), and using QPE,
+    we can express the eigenvalue exactly using only 3 register qubits.
+*/
+
+/*
+    This function applies the unitary operator of interest (the T gate) to the circuit
+    in the manner required for QPE.
+
+    More specifically, it applies the T gate in a multiplicity-binary controlled manner,
+    where the gate is applied `2^n` times for the `n`th register qubit.
+*/
 void apply_multiplicity_controlled_t_gate_manually(mqis::QuantumCircuit& circuit)
 {
     const auto angle = M_PI_4;
+
+    // apply the T gate:
+    // - 1 time  for the 0th register qubit
+    // - 2 times for the 1st register qubit
+    // - 3 times for the 2nd register qubit
     circuit.add_cp_gate({{0, 3, angle}});
     circuit.add_cp_gate({{1, 3, angle}, {1, 3, angle}});
     circuit.add_cp_gate({{2, 3, angle}, {2, 3, angle}, {2, 3, angle}, {2, 3, angle}});
@@ -15,28 +45,36 @@ void apply_multiplicity_controlled_t_gate_manually(mqis::QuantumCircuit& circuit
 
 auto main() -> int
 {
-    auto state = mqis::QuantumState {"0000"};
+    // specify the number of register and unitary qubits for the problem
+    const auto n_register_qubits = 3;
+    const auto n_unitary_qubits = 1;
 
-    const auto n_counting_qubits = 3;
-    const auto n_ancilla_qubits = 1;
-
-    auto circuit = mqis::QuantumCircuit {n_counting_qubits + n_ancilla_qubits};
+    // create the circuit with the gates needed to perform QPE
+    auto circuit = mqis::QuantumCircuit {n_register_qubits + n_unitary_qubits};
     circuit.add_h_gate({0, 1, 2});
     circuit.add_x_gate(3);
     apply_multiplicity_controlled_t_gate_manually(circuit);
     mqis::apply_inverse_fourier_transform(circuit, {2, 1, 0});
 
-    mqis::simulate(circuit, state);
+    // construct the statevector in the 0-state, and propagate it through the circuit
+    auto statevector = mqis::QuantumState {"0000"};
+    mqis::simulate(circuit, statevector);
 
-    const auto counts = mqis::perform_measurements_as_counts_marginal(state, 1024, {3});
+    // get a map of the bitstrings to the counts; in QPE, we are concerned with the output
+    // of the register qubits, and thus we marginalize the unitary qubit (`3`) here
+    const auto counts = mqis::perform_measurements_as_counts_marginal(statevector, 1024, {3});
 
     for (const auto& [bitstring, count]: counts) {
         std::cout << "(state, count) = (" << bitstring << ", " << count << ")\n";
 
-        const auto rstripped_bitstring = mqis::rstrip_marginal_bits(bitstring);
-        const auto state_index = mqis::bitstring_to_state_index_little_endian(rstripped_bitstring);
-        const auto n_states = 1ul << n_counting_qubits;
-        const auto binary_fraction = static_cast<double>(state_index) / static_cast<double>(n_states);
+        auto rstripped_bitstring = mqis::rstrip_marginal_bits(bitstring);
+
+        // the manner in which we apply the controlled unitary gates for QPE affects the output;
+        // - in this example, the 0th qubit was applied once, the 1st qubit was applied twice, etc.
+        // - this means the largest contributor is on the right of the bitstring
+        //   - and we need to reverse the bitstring being calculating the binary fraction expansion
+        std::ranges::reverse(rstripped_bitstring);
+        const auto binary_fraction = mqis::binary_fraction_expansion(rstripped_bitstring);
 
         const auto estimated_phase = 2.0 * M_PI * binary_fraction;
 
@@ -45,6 +83,7 @@ auto main() -> int
     // Expected output:
     // ```
     // (state, count) = (100x, 1024)
+    // estimated phase: 0.785398
     // ```
 
     return 0;
