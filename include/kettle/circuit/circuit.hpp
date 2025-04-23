30248dbe4e349e1069cfc55a635c7f4fe0b4b2b6
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-04-23 16:55:44 -0400

    Move CircuitElement into its own header

--- include/kettle/circuit/circuit.hpp
+++ include/kettle/circuit/circuit.hpp
@@ -17,52 +17,8 @@
 #include "kettle/gates/common_u_gates.hpp"
 #include "kettle/gates/primitive_gate.hpp"
 #include "kettle/circuit/control_flow.hpp"
+#include "kettle/circuit/circuit_element.hpp"
 
-
-namespace impl_ket
-{
-
-/*
-    A thin wrapper around `std::variant`, with member functions to make it more
-    convenient to work with.
-*/
-struct CircuitElement
-{
-public:
-    CircuitElement(const ket::GateInfo& ginfo)
-        : element_ {ginfo}
-    {}
-
-    CircuitElement(ControlFlowInstruction instruction)
-        : element_ {std::move(instruction)}
-    {}
-
-    constexpr auto is_gate() const -> bool
-    {
-        return std::holds_alternative<ket::GateInfo>(element_);
-    }
-
-    constexpr auto is_control_flow() const -> bool
-    {
-        return std::holds_alternative<ControlFlowInstruction>(element_);
-    }
-
-    constexpr auto get_gate() const -> const ket::GateInfo&
-    {
-        return std::get<ket::GateInfo>(element_);
-    }
-
-    constexpr auto get_control_flow() const -> const ControlFlowInstruction&
-    {
-        return std::get<ControlFlowInstruction>(element_);
-    }
-
-private:
-    std::variant<ket::GateInfo, ControlFlowInstruction> element_;
-};
-
-
-}  // namespace impl_ket
 
 namespace ket
 {
---
+++ include/kettle/circuit/circuit_element.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <variant>
+
+#include "kettle/gates/primitive_gate.hpp"
+#include "kettle/circuit/control_flow.hpp"
+
+/*
+    This header file contains the CircuitElement type, which holds all the different
+    constructs that determine how the quantum circuit will evolve (gates, control flow
+    instructions, etc.).
+*/
+
+
+namespace impl_ket
+{
+
+/*
+    A thin wrapper around `std::variant`, with member functions to make it more
+    convenient to work with.
+*/
+struct CircuitElement
+{
+public:
+    CircuitElement(const ket::GateInfo& ginfo)
+        : element_ {ginfo}
+    {}
+
+    CircuitElement(ControlFlowInstruction instruction)
+        : element_ {std::move(instruction)}
+    {}
+
+    constexpr auto is_gate() const -> bool
+    {
+        return std::holds_alternative<ket::GateInfo>(element_);
+    }
+
+    constexpr auto is_control_flow() const -> bool
+    {
+        return std::holds_alternative<ControlFlowInstruction>(element_);
+    }
+
+    constexpr auto get_gate() const -> const ket::GateInfo&
+    {
+        return std::get<ket::GateInfo>(element_);
+    }
+
+    constexpr auto get_control_flow() const -> const ControlFlowInstruction&
+    {
+        return std::get<ControlFlowInstruction>(element_);
+    }
+
+private:
+    std::variant<ket::GateInfo, ControlFlowInstruction> element_;
+};
+
+
+}  // namespace impl_ket
