22276e189670ff0c4cafc2b1efd783da3dcd4ed0
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-04-30 01:21:36 -0400

    Add circuit logger type

---
+++ include/kettle/circuit_loggers/circuit_logger.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <variant>
+
+#include "kettle/circuit_loggers/classical_register_circuit_logger.hpp"
+
+namespace ket
+{
+
+class CircuitLogger
+{
+public:
+    // NOLINTNEXTLINE(*explicit*)
+    CircuitLogger(ClassicalRegisterCircuitLogger circuit_logger)
+        : circuit_logger_ {std::move(circuit_logger)}
+    {}
+
+    [[nodiscard]]
+    constexpr auto is_classical_register_circuit_logger() const -> bool
+    {
+        return std::holds_alternative<ClassicalRegisterCircuitLogger>(circuit_logger_);
+    }
+
+    [[nodiscard]]
+    constexpr auto get_classical_register_circuit_logger() const -> const ClassicalRegisterCircuitLogger&
+    {
+        return std::get<ClassicalRegisterCircuitLogger>(circuit_logger_);
+    }
+
+private:
+    std::variant<ClassicalRegisterCircuitLogger> circuit_logger_;
+};
+
+}  // namespace ket
