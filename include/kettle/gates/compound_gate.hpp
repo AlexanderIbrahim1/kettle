00c096db51d4946d5203dd3fb6419cb5ab84aba7
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-06-03 22:48:53 -0400

    Rename gate type

--- include/kettle/gates/compound_gate.hpp
+++ include/kettle/gates/compound_gate.hpp
@@ -7,11 +7,11 @@ namespace ket
 {
 
 /*
-    The `GeneralGate` type represents quantum gates that are not directly simulated on a
+    The `CompoundGate` type represents quantum gates that are not directly simulated on a
     simulator. A `GeneralGate` must be decomposed into one or more `Gate` instances to be
     simulated.
 */
-enum class GeneralGate : std::uint8_t
+enum class CompoundGate : std::uint8_t
 {
     CCX,
     CCY,
