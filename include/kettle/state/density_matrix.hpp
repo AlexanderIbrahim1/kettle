e7c79203c56ad03901e5c97e168813da78d27375
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2025-06-20 23:30:58 -0400

    Split density matrix into header and source

--- include/kettle/state/density_matrix.hpp
+++ include/kettle/state/density_matrix.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <stdexcept>
-
 #include <Eigen/Dense>
 
 #include "kettle/common/tolerance.hpp"
@@ -16,137 +14,51 @@
 //       - for example, a function for probabilistic mixtures, another for doing something -> renormalizing
 //     - another is to just get rid of the invariant checking
 
-// TODO: remove this once I get it to work; I'll be doing the header/source split
-inline auto log_2_int(std::size_t power) noexcept -> std::size_t
-{
-    // assumes that power > 0
-    auto log2 = std::size_t {0};
-    while (power > 0) {
-        ++log2;
-        power = (power >> 1UL);
-    }
-
-    return log2 - 1;
-}
-
-
 namespace ket
 {
 
-inline void check_is_hermitian_(const Eigen::MatrixXcd& matrix, double tolerance = MATRIX_HERMITIAN_TOLERANCE) {
-    if (!matrix.isApprox(matrix.adjoint(), tolerance)) {
-        throw std::runtime_error {"ERROR: provided matrix is not Hermitian.\n"};
-    }
-}
-
-inline void check_is_finite_size_(const Eigen::MatrixXcd& matrix)
-{
-    if (matrix.size() == 0) {
-        throw std::runtime_error {"ERROR: provided matrix has a size of 0 x 0.\n"};
-    }
-}
-
-inline void check_is_square_matrix_(const Eigen::MatrixXcd& matrix)
-{
-    if (matrix.cols() != matrix.rows()) {
-        throw std::runtime_error {"ERROR: provided matrix is not square.\n"};
-    }
-}
-
-inline void check_has_trace_of_one_(const Eigen::MatrixXcd& matrix, double tolerance = DENSITY_MATRIX_TRACE_TOLERANCE)
-{
-    const auto trace = matrix.trace();
-    const auto unity = std::complex<double> {1.0, 0.0};
-
-    if (std::norm(trace - unity) > tolerance) {
-        throw std::runtime_error {"ERROR: provided matrix does not have a trace of 1.\n"};
-    }
-}
-
-inline void check_is_positive_semi_definite_(const Eigen::MatrixXcd& matrix)
-{
-    // perform the LDL^T decomposition, which should be faster than finding all the eigenvalues
-    // an earlier check should have determined if the matrix is Hermitian
-    const auto ldlt = Eigen::LDLT<Eigen::MatrixXcd> {matrix};
-    if (!ldlt.isPositive() || (ldlt.info() != Eigen::Success)) {
-        throw std::runtime_error {"ERROR: provided matrix is not positive semidefinite.\n"};
-    }
-}
-
-inline void check_side_length_is_power_of_2_(const Eigen::MatrixXcd& matrix)
-{
-    const auto size = static_cast<std::size_t>(matrix.cols());
-    const auto is_positive = size > 0;
-    const auto has_one_bit_set = (size & (size - 1)) == 0;
-
-    if (!is_positive || !has_one_bit_set) {
-        auto err_msg = std::stringstream {};
-        err_msg << "The provided coefficients must have a size equal to a power of 2.\n";
-        err_msg << "Found size = " << matrix.cols();
-        throw std::runtime_error {err_msg.str()};
-    }
-}
-
-struct density_matrix_nocheck
-{
-    explicit density_matrix_nocheck() = default;
-};
+struct density_matrix_nocheck {};
 
 class DensityMatrix
 {
 public:
-    explicit DensityMatrix(
-        Eigen::MatrixXcd matrix,  // NOLINT(modernize-pass-by-value) [reason: fails for Eigen]
-        double trace_tolerance = DENSITY_MATRIX_TRACE_TOLERANCE,
-        double hermitian_tolerance = MATRIX_HERMITIAN_TOLERANCE
-    )
-        : n_qubits_ {log_2_int(static_cast<std::size_t>(matrix.cols()))}
-        , n_states_ {static_cast<std::size_t>(matrix.cols())}
-        , matrix_ {std::move(matrix)}
-    {
-        check_is_finite_size_(matrix_);
-        check_is_square_matrix_(matrix_);
-        check_has_trace_of_one_(matrix_, trace_tolerance);
-        check_is_hermitian_(matrix_, hermitian_tolerance);
-        check_is_positive_semi_definite_(matrix_);
-        check_side_length_is_power_of_2_(matrix_);
-    }
-
     /*
-        A version of the constructor where the checks that determine if a density matrix is valid, are
-        all skipped.
+        Construct the `DensityMatrix` type from an existing `Eigen::MatrixXcd` instance `matrix`.
+        
+        This constructor checks if the instance provided is a valid density matrix. It checks if
+        the matrix is:
+          - a square matrix with a side length equal to a positive power of 2
+          - Hermitian (such that `|matrix - matrix.adjoint()| < hermitian_tolerance`)
+          - of unit trace (such that `|Trace(matrix) - 1|^2 < trace_tolerance`)
+          - positive semi-definite
     */
     explicit DensityMatrix(
-        Eigen::MatrixXcd matrix,  // NOLINT(modernize-pass-by-value) [reason: fails for Eigen] 
-        [[maybe_unused]] const density_matrix_nocheck& key
-    )
-        : n_qubits_ {log_2_int(static_cast<std::size_t>(matrix.cols()))}
-        , n_states_ {static_cast<std::size_t>(matrix.cols())}
-        , matrix_ {std::move(matrix)}
-    {}
+        Eigen::MatrixXcd matrix,
+        double trace_tolerance = DENSITY_MATRIX_TRACE_TOLERANCE,
+        double hermitian_tolerance = MATRIX_HERMITIAN_TOLERANCE
+    );
 
+    /*
+        Construct the `DensityMatrix` type from an existing `Eigen::MatrixXcd` instance `matrix`.
+
+        All checks that determine if a density matrix is valid are all skipped.
+    */
+    explicit DensityMatrix(
+        Eigen::MatrixXcd matrix,
+        [[maybe_unused]] const density_matrix_nocheck& key
+    );
+
+    /*
+        Construct the `DensityMatrix` type for a single computational basis state, represented by
+        the bitstring `computational_state`. The endianness of the input is provided by `input_endian`.
+
+        The resulting density matrix will have a 0 for every entry, except for the diagonal element
+        given by the index calculated from the bistring.
+    */
     explicit DensityMatrix(
         const std::string& computational_state,
         Endian input_endian = Endian::LITTLE
-    )
-        : n_qubits_ {computational_state.size()}
-        , n_states_ {1UL << computational_state.size()}
-        , matrix_(Eigen::MatrixXcd::Zero(static_cast<Eigen::Index>(1UL << computational_state.size()), static_cast<Eigen::Index>(1UL << computational_state.size())).eval())  // TODO: CLEAN UP
-    {
-        const auto index = static_cast<Eigen::Index>(bitstring_to_state_index(computational_state, input_endian)); // TODO: CLEAN UP
-        matrix_(index, index) = 1.0;
-    }
-
-    [[nodiscard]]
-    auto is_pure(double tolerance = DENSITY_MATRIX_TRACE_TOLERANCE) const -> bool
-    {
-        const auto square = matrix_ * matrix_;
-
-        const auto trace = square.trace();
-        const auto unity = std::complex<double> {1.0, 0.0};
-
-        return std::norm(trace - unity) <= tolerance;
-    }
+    );
 
     [[nodiscard]]
     constexpr auto matrix() const -> const Eigen::MatrixXcd&
@@ -161,19 +73,6 @@ public:
     }
 
     [[nodiscard]]
-    auto trace() const -> std::complex<double>
-    {
-        return matrix_.trace();
-    }
-
-    [[nodiscard]]
-    auto trace_of_square() const -> std::complex<double>
-    {
-        const auto squared = matrix_ * matrix_;
-        return squared.trace();
-    }
-
-    [[nodiscard]]
     constexpr auto n_states() const noexcept -> std::size_t
     {
         return n_states_;
@@ -185,29 +84,34 @@ public:
         return n_qubits_;
     }
 
+    /*
+        Check if the trace of the square of the density matrix is equal to unity, such that
+            `|Trace(matrix^2) - 1| <= tolerance`
+    */
+    [[nodiscard]]
+    auto is_pure(double tolerance = DENSITY_MATRIX_TRACE_TOLERANCE) const -> bool;
+
+    /*
+        Get the trace of the matrix.
+    */
+    [[nodiscard]]
+    auto trace() const -> std::complex<double>;
+
+    /*
+        Get the trace of the square of the matrix.
+    */
+    [[nodiscard]]
+    auto trace_of_square() const -> std::complex<double>;
+
 private:
     std::size_t n_qubits_;
     std::size_t n_states_;
     Eigen::MatrixXcd matrix_;
 };
 
-
-inline auto statevector_to_density_matrix(const Statevector& statevector) -> DensityMatrix
-{
-    const auto n_states = statevector.n_states();
-
-    auto dens_mat = Eigen::MatrixXcd(n_states, n_states);
-
-    for (std::size_t i0 {0}; i0 < n_states; ++i0) {
-        const auto idx0 = static_cast<Eigen::Index>(i0);
-        for (std::size_t i1 {0}; i1 < n_states; ++i1) {
-            const auto idx1 = static_cast<Eigen::Index>(i1);
-            dens_mat(idx0, idx1) = statevector[i0] * std::conj(statevector[i1]);
-        }
-    }
-
-    return DensityMatrix {std::move(dens_mat), density_matrix_nocheck {}};
-}
-
+/*
+    Convert the statevector to a density matrix.
+*/
+auto statevector_to_density_matrix(const Statevector& statevector) -> DensityMatrix;
 
 }  // namespace ket
---
+++ source/kettle_internal/state/density_matrix.cpp
@@ -0,0 +1,140 @@
+#include <stdexcept>
+
+#include <Eigen/Dense>
+
+#include "kettle/state/density_matrix.hpp"
+#include "kettle/state/statevector.hpp"
+
+#include "kettle_internal/common/mathtools_internal.hpp"
+
+namespace ki = ket::internal;
+
+namespace
+{
+
+void check_is_hermitian_(const Eigen::MatrixXcd& matrix, double tolerance) {
+    if (!matrix.isApprox(matrix.adjoint(), tolerance)) {
+        throw std::runtime_error {"ERROR: provided matrix is not Hermitian.\n"};
+    }
+}
+
+void check_is_finite_size_(const Eigen::MatrixXcd& matrix)
+{
+    if (matrix.size() == 0) {
+        throw std::runtime_error {"ERROR: provided matrix has a size of 0 x 0.\n"};
+    }
+}
+
+void check_is_square_matrix_(const Eigen::MatrixXcd& matrix)
+{
+    if (matrix.cols() != matrix.rows()) {
+        throw std::runtime_error {"ERROR: provided matrix is not square.\n"};
+    }
+}
+
+void check_has_trace_of_one_(const Eigen::MatrixXcd& matrix, double tolerance)
+{
+    const auto trace = matrix.trace();
+    const auto unity = std::complex<double> {1.0, 0.0};
+
+    if (std::norm(trace - unity) > tolerance) {
+        throw std::runtime_error {"ERROR: provided matrix does not have a trace of 1.\n"};
+    }
+}
+
+void check_is_positive_semi_definite_(const Eigen::MatrixXcd& matrix)
+{
+    // perform the LDL^T decomposition, which should be faster than finding all the eigenvalues
+    // an earlier check should have determined if the matrix is Hermitian
+    const auto ldlt = Eigen::LDLT<Eigen::MatrixXcd> {matrix};
+    if (!ldlt.isPositive() || (ldlt.info() != Eigen::Success)) {
+        throw std::runtime_error {"ERROR: provided matrix is not positive semidefinite.\n"};
+    }
+}
+
+void check_side_length_is_power_of_2_(const Eigen::MatrixXcd& matrix)
+{
+    const auto size = static_cast<std::size_t>(matrix.cols());
+    const auto is_positive = size > 0;
+    const auto has_one_bit_set = (size & (size - 1)) == 0;
+
+    if (!is_positive || !has_one_bit_set) {
+        auto err_msg = std::stringstream {};
+        err_msg << "The provided coefficients must have a size equal to a power of 2.\n";
+        err_msg << "Found size = " << matrix.cols();
+        throw std::runtime_error {err_msg.str()};
+    }
+}
+
+}  // namespace
+
+
+namespace ket
+{
+
+DensityMatrix::DensityMatrix(Eigen::MatrixXcd matrix, double trace_tolerance, double hermitian_tolerance)
+    : n_qubits_ {ki::log_2_int(static_cast<std::size_t>(matrix.cols()))}
+    , n_states_ {static_cast<std::size_t>(matrix.cols())}
+    , matrix_ {std::move(matrix)}
+{
+    check_is_finite_size_(matrix_);
+    check_is_square_matrix_(matrix_);
+    check_has_trace_of_one_(matrix_, trace_tolerance);
+    check_is_hermitian_(matrix_, hermitian_tolerance);
+    check_is_positive_semi_definite_(matrix_);
+    check_side_length_is_power_of_2_(matrix_);
+}
+
+DensityMatrix::DensityMatrix(Eigen::MatrixXcd matrix, [[maybe_unused]] const density_matrix_nocheck& key)
+    : n_qubits_ {ki::log_2_int(static_cast<std::size_t>(matrix.cols()))}
+    , n_states_ {static_cast<std::size_t>(matrix.cols())}
+    , matrix_ {std::move(matrix)}
+{}
+
+DensityMatrix::DensityMatrix(const std::string& computational_state, Endian input_endian)
+    : n_qubits_ {computational_state.size()}
+    , n_states_ {1UL << computational_state.size()}
+    , matrix_(Eigen::MatrixXcd::Zero(static_cast<Eigen::Index>(1UL << computational_state.size()), static_cast<Eigen::Index>(1UL << computational_state.size())).eval())  // TODO: CLEAN UP
+{
+    const auto index = static_cast<Eigen::Index>(bitstring_to_state_index(computational_state, input_endian)); // TODO: CLEAN UP
+    matrix_(index, index) = 1.0;
+}
+
+[[nodiscard]]
+auto DensityMatrix::is_pure(double tolerance) const -> bool
+{
+    const auto unity = std::complex<double> {1.0, 0.0};
+    return std::norm(trace_of_square() - unity) <= tolerance;
+}
+
+[[nodiscard]]
+auto DensityMatrix::trace() const -> std::complex<double>
+{
+    return matrix_.trace();
+}
+
+[[nodiscard]]
+auto DensityMatrix::trace_of_square() const -> std::complex<double>
+{
+    const auto squared = matrix_ * matrix_;
+    return squared.trace();
+}
+
+auto statevector_to_density_matrix(const Statevector& statevector) -> DensityMatrix
+{
+    const auto n_states = statevector.n_states();
+
+    auto dens_mat = Eigen::MatrixXcd(n_states, n_states);
+
+    for (std::size_t i0 {0}; i0 < n_states; ++i0) {
+        const auto idx0 = static_cast<Eigen::Index>(i0);
+        for (std::size_t i1 {0}; i1 < n_states; ++i1) {
+            const auto idx1 = static_cast<Eigen::Index>(i1);
+            dens_mat(idx0, idx1) = statevector[i0] * std::conj(statevector[i1]);
+        }
+    }
+
+    return DensityMatrix {std::move(dens_mat), density_matrix_nocheck {}};
+}
+
+}  // namespace ket
